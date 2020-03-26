4549fe92a55c859b86db48c7d7de380a984ad820
luokaiii <133@163.com>
2020-03-26 14:23:51 +0800

    更改注册页

--- frontend/src/pages/frontend/me/registry.jsx
+++ frontend/src/pages/frontend/me/registry.jsx
@@ -7,12 +7,15 @@ import {
   Row,
   Col,
   InputNumber,
-  message
+  message,
+  Steps,
+  Result
 } from "antd";
 
 import "./registry.less";
 
 export default Form.create()(({ form }) => {
+  const [current, setCurrent] = useState(0);
   const [authCode, setAuthCode] = useState(
     Math.floor(Math.random() * 9000) + 1000
   );
@@ -58,115 +61,156 @@ export default Form.create()(({ form }) => {
     e.preventDefault();
     form.validateFieldsAndScroll((err, values) => {
       if (!err) {
-        message.success("注册成功，2秒后跳转至登录页");
-        setTimeout(() => {
-          window.location.href = "/#/f/login";
-        }, 1500);
+        setCurrent(2);
       }
     });
+  };
+
+  const Step1Render = () => {
+    return (
+      <div>
+        <Form.Item>
+          {getFieldDecorator("username", {
+            rules: [
+              { required: true, message: "用户名不能为空" },
+              { validator: checkUsername }
+            ]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="用户名 为6-18位字母、数字组合"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("nickname", {
+            rules: [{ required: true, message: "昵称不能为空" }]
+          })(
+            <Input
+              prefix={
+                <Icon type="smile" style={{ color: "rgba(0,0,0,.25)" }} />
+              }
+              placeholder="昵称 为2-12位字符"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("phone", {
+            rules: [{ required: true, message: "联系方式不能为空" }]
+          })(
+            <Input
+              prefix={
+                <Icon type="phone" style={{ color: "rgba(0,0,0,.25)" }} />
+              }
+              placeholder="请输入联系方式"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("password", {
+            rules: [
+              { required: true, message: "密码不能为空" },
+              { validator: checkPassword }
+            ]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="密码 为6-18位字母、数字组合"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("password2", {
+            rules: [
+              { required: true, message: "请再次输入密码" },
+              { validator: checkConfirmPassword }
+            ]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="请再次输入密码"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("auth-code", {
+            rules: [
+              { required: true, message: "验证码不能为空" },
+              { validator: checkAuthCode }
+            ]
+          })(
+            <Row>
+              <Col span={16}>
+                <InputNumber placeholder="请输入验证码" max={9999} />
+              </Col>
+              <Col span={8}>
+                <div className="auth-code" onClick={updateCode}>
+                  {authCode}
+                </div>
+              </Col>
+            </Row>
+          )}
+        </Form.Item>
+      </div>
+    );
+  };
+
+  const Step2Render = () => {
+    return (
+      <Form.Item>
+        <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+          注 册
+        </Button>
+      </Form.Item>
+    );
+  };
+
+  const Step3Render = () => {
+    return (
+      <Result
+        status="success"
+        title="注册成功"
+        extra={[
+          <Button key="login" href="/#/f/login">
+            去登陆
+          </Button>
+        ]}
+      />
+    );
   };
 
   return (
     <div className="registry">
       <div style={{ height: "70px" }}></div>
       <div className="login-div">
-        <h2 className="title">注 册</h2>
+        <h3 className="title">注册并填写信息</h3>
         <Form className="form" onSubmit={handleSubmit}>
-          <Form.Item>
-            {getFieldDecorator("username", {
-              rules: [
-                { required: true, message: "用户名不能为空" },
-                { validator: checkUsername }
-              ]
-            })(
-              <Input
-                prefix={
-                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                placeholder="用户名 为6-18位字母、数字组合"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator("nickname", {
-              rules: [{ required: true, message: "昵称不能为空" }]
-            })(
-              <Input
-                prefix={
-                  <Icon type="smile" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                placeholder="昵称 为2-12位字符"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator("phone", {
-              rules: [{ required: true, message: "联系方式不能为空" }]
-            })(
-              <Input
-                prefix={
-                  <Icon type="phone" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                placeholder="请输入联系方式"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator("password", {
-              rules: [
-                { required: true, message: "密码不能为空" },
-                { validator: checkPassword }
-              ]
-            })(
-              <Input
-                prefix={
-                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                type="password"
-                placeholder="密码 为6-18位字母、数字组合"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator("password2", {
-              rules: [
-                { required: true, message: "请再次输入密码" },
-                { validator: checkConfirmPassword }
-              ]
-            })(
-              <Input
-                prefix={
-                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                type="password"
-                placeholder="请再次输入密码"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator("auth-code", {
-              rules: [
-                { required: true, message: "验证码不能为空" },
-                { validator: checkAuthCode }
-              ]
-            })(
-              <Row>
-                <Col span={16}>
-                  <InputNumber placeholder="请输入验证码" max={9999} />
-                </Col>
-                <Col span={8}>
-                  <div className="auth-code" onClick={updateCode}>
-                    {authCode}
-                  </div>
-                </Col>
-              </Row>
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
-              注 册
+          <Steps current={current}>
+            <Steps.Step title="注册" />
+            <Steps.Step title="完善" />
+            <Steps.Step title="完成" />
+          </Steps>
+          {current === 0 && <Step1Render />}
+          {current === 1 && <Step2Render />}
+          {current === 2 && <Step3Render />}
+          {current === 0 && (
+            <Button block type="primary" onClick={() => setCurrent(1)}>
+              下一步
             </Button>
-          </Form.Item>
+          )}
+          {current === 1 && (
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: "100%" }}
+              >
+                注 册
+              </Button>
+            </Form.Item>
+          )}
         </Form>
       </div>
     </div>
